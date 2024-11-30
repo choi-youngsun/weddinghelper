735da1d52a5bd82f9db30b267f6505e751d13209
Choi Young Sun <sunn7931@naver.com>
2024-11-30 17:08:37 +0900

    chore: 초기 세팅

---
+++ src/pages/setting/index.tsx
@@ -0,0 +1,3 @@
+export default function Setting() {
+  return <p>설정 페이지</p>;
+}
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,8 @@
+export default function SignUp() {
+  return (
+    <>
+      <p>회원가입 페이지</p>
+      <p>프리티어 안되나?</p>
+    </>
+  );
+}
