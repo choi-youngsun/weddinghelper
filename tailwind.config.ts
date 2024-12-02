cd3954cc9b543447524a7e6c5a92c3a909b66c92
Choi Young Sun <sunn7931@naver.com>
2024-12-03 04:53:24 +0900

    chore: 테일윈드 폰트 스타일 변수 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,18 @@ export default {
 
   theme: {
     extend: {
+      boxShadow: {
+        'right-bottom': '4px 4px 6px rgba(0, 0, 0, 0.2)',
+      },
+      fontSize: {
+        'sm-regular': ['16px', { lineHeight: '1.5', fontWeight: '400' }], // Regular
+        'sm-extraBold': ['16px', { lineHeight: '1.5', fontWeight: '800' }], // ExtraBold
+        'md-regular': ['20px', { lineHeight: '1.5', fontWeight: '400' }], // Regular
+        'md-extraBold': ['20px', { lineHeight: '1.5', fontWeight: '800' }], // ExtraBold
+        'lg-regular': ['24px', { lineHeight: '1.5', fontWeight: '400' }], // Regular
+        'xl-regular': ['32px', { lineHeight: '1.5', fontWeight: '400' }], // Regular
+        '2xl-regular': ['40px', { lineHeight: '1.5', fontWeight: '400' }], // Regular
+      },
       colors: {
         text: {
           white: '#FFFFFF',
