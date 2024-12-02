cac947f05110177c4b4fb225edac5b866707f987
Choi Young Sun <sunn7931@naver.com>
2024-12-03 04:08:20 +0900

    feat: 테스트 페이지 추가

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,10 @@
+export default function Test() {
+  return (
+    <main className="grid grid-cols-3">
+      <div>랜딩 페이지</div>
+      <div>랜딩 페이지</div>
+      <div>랜딩 페이지</div>
+      <div>랜딩 페이지</div>
+    </main>
+  );
+}
