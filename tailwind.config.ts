a456361e6d39bfae7bc27497ff8094159d26269e
Choi Young Sun <sunn7931@naver.com>
2024-12-03 03:38:38 +0900

    chore: 색상 변수 세팅

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
@@ -6,8 +6,28 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        text: {
+          white: '#FFFFFF',
+          gray: '#626060',
+          orange: '#FF811A',
+        },
+        background: {
+          blue: '#DEE5FF',
+          pink: '#FFECEC',
+          yellow: '#FFF8D1',
+        },
+        button: {
+          green: '#00AD2E',
+          blue: '#B9C2FF',
+          disabled: '#D2D2D2',
+          red: '#FF3F3F',
+          pink: '#FFB9B9',
+          yellow: '#FFD641',
+        },
+        pressed: {
+          yellow: '#F6CC36',
+          pink: '#FF8C8C',
+        },
       },
     },
   },
