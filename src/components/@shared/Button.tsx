1eeed5e2b7687a3c38c3c67be9e6b793760d7d14
Choi Young Sun <sunn7931@naver.com>
2024-12-03 20:54:04 +0900

    docs: 버튼 컴포넌트에 JSDoc 추가

--- src/components/@shared/Button.tsx
+++ src/components/@shared/Button.tsx
@@ -13,6 +13,35 @@ type ButtonProps = {
   children: React.ReactNode;
 } & React.ComponentPropsWithoutRef<'button'>;
 
+/**
+ * 다양한 스타일링 옵션을 제공하는 커스터마이징 가능한 버튼 컴포넌트입니다.
+ *
+ * @param {Object} props - 버튼 컴포넌트의 속성입니다.
+ * @param {'square' | 'round' | 'left-round'} [props.buttonStyle='square'] - 버튼의 모양을 정의합니다.
+ *        - `square`: 기본 스타일로 약간의 둥근 모서리를 가진 사각형 버튼입니다.
+ *        - `round`: 전체가 둥근 모양의 버튼입니다.
+ *        - `left-round`: 좌측 모서리만 둥근 버튼입니다.
+ * @param {'fitToChildren' | 'fitToParent'} [props.buttonWidth='fitToParent'] - 버튼의 너비를 정의합니다.
+ *        - `fitToChildren`: 버튼 내용에 맞춰 너비가 조정됩니다.
+ *        - `fitToParent`: 부모 요소의 너비를 채웁니다.
+ * @param {'yellow' | 'red' | 'pink' | 'blue' | 'green' | 'white'} [props.buttonColor='yellow'] - 버튼의 배경색을 정의합니다.
+ * @param {'white' | 'black'} [props.textColor='black'] - 버튼 텍스트의 색상을 정의합니다.
+ * @param {'16' | '20' | '24'} [props.textSize='16'] - 텍스트의 크기를 정의합니다.
+ *        - `16`: 작은 크기의 텍스트입니다.
+ *        - `20`: 중간 크기의 텍스트입니다.
+ *        - `24`: 큰 크기의 텍스트입니다.
+ * @param {'regular' | 'bold'} [props.textWeight='regular'] - 텍스트의 굵기를 정의합니다.
+ * @param {'yellow' | 'gray' | 'none' | 'shadow'} [props.borderColor='none'] - 버튼 테두리의 색상 또는 그림자 효과를 정의합니다.
+ * @param {number} [props.buttonHeight=50] - 버튼의 높이를 정의합니다(단위: 픽셀).
+ * @param {string} [props.className] - 추가적인 클래스명을 설정할 수 있습니다.
+ * @param {React.ReactNode} props.children - 버튼 내부에 렌더링할 내용을 정의합니다.
+ * @param {React.CSSProperties} [props.style] - 추가적인 인라인 스타일을 정의할 수 있습니다.
+ * @param {boolean} [props.disabled=false] - 버튼의 비활성화 여부를 설정합니다.
+ * @param {Object} [props.buttonAttributes] - 추가적으로 전달할 버튼의 속성입니다.
+ *
+ * @returns {JSX.Element} 커스터마이징된 버튼 요소를 반환합니다.
+ */
+
 export default function Button({
   buttonStyle = 'square',
   buttonWidth = 'fitToParent',
@@ -47,7 +76,7 @@ export default function Button({
       'border-text-gray border-[1px]': borderColor === 'gray',
       'shadow-right-bottom': borderColor === 'shadow',
       'rounded-[12px]': buttonStyle === 'square',
-      'rounded-[16px]': buttonStyle === 'round',
+      'rounded-[20px]': buttonStyle === 'round',
       'rounded-bl-[16px] rounded-tl-[16px]': buttonStyle === 'left-round',
       'text-text-black': textColor === 'black',
       'text-text-white': textColor === 'white',
