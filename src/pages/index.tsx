a2c9c30b6b807a723d8ca6c1cf0a823392260ec4
Choi Young Sun <sunn7931@naver.com>
2024-12-06 03:38:28 +0900

    feat: 랜딩 페이지 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import Button from '@/components/@shared/Button';
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>랜딩 페이지</div>;
+  return (
+    <main>
+      <div className="mx-auto mt-[300px] flex w-[200px] flex-col items-center justify-center gap-[20px]">
+        <Link href={'/admin'} className="w-full">
+          <Button buttonColor="white" borderColor="shadow">
+            관리자
+          </Button>
+        </Link>
+        <Link href={'/guest/bride'} className="w-full">
+          <Button buttonColor="white" borderColor="shadow">
+            신부측 하객
+          </Button>
+        </Link>
+        <Link href={'/guest/broom'} className="w-full">
+          <Button buttonColor="white" borderColor="shadow">
+            신랑측 하객
+          </Button>
+        </Link>
+      </div>
+    </main>
+  );
 }
