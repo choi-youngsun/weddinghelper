035414850da6dbdd2184d2ccaad979797f90ca0c
Choi Young Sun <sunn7931@naver.com>
2024-12-08 20:58:32 +0900

    feat: useRadioButton 훅으로 라디오 버튼 조정하도록 수정

--- src/components/@shared/RadioButton.tsx
+++ src/components/@shared/RadioButton.tsx
@@ -1,31 +1,25 @@
-import { useState } from 'react';
-
-type RadioOption = {
+export type RadioOption = {
   label: string;
-  value: string | null;
+  value: string;
 };
 
 type RadioButtonProps = {
   options: RadioOption[];
   buttonSize?: number;
   gap?: number;
+  handleSelect: (value: string) => void;
+  selectedOption: string | null;
 } & React.HTMLAttributes<HTMLDivElement>;
 
 export default function RadioButton({
   options,
-  buttonSize = 20,
+  buttonSize = 30,
   gap = 10,
+  selectedOption,
+  handleSelect,
   className,
   ...rest
 }: RadioButtonProps) {
-  const [selectedOption, setSelectedOption] = useState<RadioOption | null>(
-    null
-  );
-
-  const handleSelect = (option: RadioOption) => {
-    setSelectedOption(option);
-  };
-
   return (
     <div className={className} {...rest}>
       {options.map((option) => (
@@ -35,14 +29,15 @@ export default function RadioButton({
           style={{ gap: `${gap}px` }}
         >
           <button
-            onClick={() => handleSelect(option)}
+            onClick={() => handleSelect(option.value)}
             className="relative shrink-0 rounded-full bg-white"
             style={{
               height: `${buttonSize}px`,
               width: `${buttonSize}px`,
             }}
+            type="button"
           >
-            {selectedOption === option && (
+            {selectedOption === option.value && (
               <span
                 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-black"
                 style={{
---
+++ src/hooks/useRadioButton.ts
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export const useRadioButton = () => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  const handleSelect = (value: string) => {
+    setSelectedOption(value);
+  };
+
+  return {
+    selectedOption,
+    handleSelect,
+  };
+};
