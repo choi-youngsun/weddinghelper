ab0e16217cb6e3687a69317b6bc462752b71b26b
Choi Young Sun <sunn7931@naver.com>
2024-12-10 21:20:05 +0900

    feat: 신랑, 신부 관리자 페이지 추가

--- src/pages/admin/broom.tsx
+++ src/pages/admin/broom.tsx
@@ -1,3 +1,45 @@
-export default function AdminBroom() {
-  return <p>신랑측 관리자 페이지</p>;
+import { GuestInfo } from '@/components/admin/AdminGuestForm';
+import AdminGuestTable from '@/components/admin/AdminGuestTable';
+import { broomGuest } from '@/utils/guestLists';
+import { useState } from 'react';
+
+export default function AdminBride() {
+  const [guests, setGuests] = useState<GuestInfo[]>(broomGuest); // 전체 리스트 관리
+  const [editModeId, setEditModeId] = useState<number | null>(null); // 수정 모드 상태
+
+  const handleChange = (id: number, name: string, value: string | number) => {
+    setGuests((prev) =>
+      prev.map((guest) =>
+        guest.id === id ? { ...guest, [name]: value } : guest
+      )
+    );
+  };
+
+  const handleEditClick = (id: number) => {
+    if (editModeId === id) {
+      // 저장 로직
+      console.log(guests.find((guest) => guest.id === id));
+      setEditModeId(null);
+    } else {
+      setEditModeId(id);
+    }
+  };
+
+  const handleDeleteClick = (id: number) => {
+    console.log(id, '번째 하객 삭제!');
+  };
+
+  return (
+    <div className="mx-auto mb-[100px] px-[20px] py-[80px] xl:w-[1280px]">
+      <p className="mb-[20px] text-md-regular">신랑측 관리자 페이지</p>
+      <AdminGuestTable
+        guestList={broomGuest}
+        editModeId={editModeId}
+        side="broom"
+        onChange={handleChange}
+        onEditClick={handleEditClick}
+        onDeleteClick={handleDeleteClick}
+      />
+    </div>
+  );
 }
