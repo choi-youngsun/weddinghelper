996ef35b2f9dece2153afd46c58710aa5239b8e4
Choi Young Sun <sunn7931@naver.com>
2025-01-01 21:22:28 +0900

    feat: 파비콘, 메타태그 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import NavBar from '@/components/@shared/NavBar';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -23,11 +24,56 @@ export default function App({ Component, pageProps }: AppProps) {
   };
 
   return (
-    <main className={`${backgroundColor()} mx-auto min-h-screen w-full`}>
-      <NavBar />
-      <div>
-        <Component {...pageProps} />
-      </div>
-    </main>
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>웨딩헬퍼</title>
+        <meta
+          name="description"
+          content="결혼식 방명록 정리를 쉽고 빠르게. 웨딩헬퍼와 함께해보세요"
+        />
+        <meta name="keywords" content="서비스, 웹사이트, Next.js, 오픈그래프" />
+        <meta name="author" content="최영선 choi-youngsun" />
+
+        {/* Open Graph 메타태그 */}
+        <meta property="og:title" content="웨딩헬퍼" />
+        <meta
+          property="og:description"
+          content="결혼식 방명록 정리를 쉽고 빠르게. 웨딩헬퍼와 함께해보세요"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://yourwebsite.com" />
+        <meta
+          property="og:image"
+          content="https://yourwebsite.com/og-image.png"
+        />
+        <meta property="og:site_name" content="웨딩헬퍼" />
+        <meta property="og:locale" content="ko_KR" />
+
+        {/* Twitter 카드 메타태그 */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="웨딩헬퍼" />
+        <meta
+          name="twitter:description"
+          content="결혼식 방명록 정리를 쉽고 빠르게. 웨딩헬퍼와 함께해보세요"
+        />
+        <meta
+          name="twitter:image"
+          content="https://yourwebsite.com/twitter-image.png"
+        />
+        <meta name="twitter:site" content="@YourTwitterHandle" />
+
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+      <main className={`${backgroundColor()} mx-auto min-h-screen w-full`}>
+        <NavBar />
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </>
   );
 }
