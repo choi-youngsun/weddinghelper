45652da56a9283ee2dfa69bf53a636b9f404ac8b
Choi Young Sun <sunn7931@naver.com>
2025-01-07 17:18:40 +0900

    rename: guest 모델을 brideGuest로 변경

--- src/db/models/brideGuest.ts
+++ src/db/models/brideGuest.ts
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 // 타입 정의 (TypeScript용)
-interface IGuest extends Document {
-  userId: number; // 사용자 ID
+export interface IBrideGuest extends Document {
+  userId: mongoose.Schema.Types.ObjectId; // 사용자 ID
   orderNumber: number; // 정렬 번호
   side: string; // 신부측, 신랑측 등
   guestName: string; // 손님 이름
-  group: string; // 그룹 (가족, 직장 등)
+  affiliation: string; // 그룹 (가족, 직장 등)
   giftAmount?: number; // 축의금 금액 (optional)
   ticketCount?: number; // 식권 수 (optional)
   note?: string; // 비고 (optional)
@@ -14,13 +14,17 @@ interface IGuest extends Document {
   updatedAt?: Date;
 }
 
-const guestSchema = new mongoose.Schema(
+const brideGuestSchema = new mongoose.Schema(
   {
-    userId: { type: Number, required: true }, // 필수
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     orderNumber: { type: Number, required: true }, // 필수
-    side: { type: String, required: true, enum: ['신부측', '신랑측'] }, // 필수, 선택값 제한
+    side: { type: String, required: true }, // 필수
     guestName: { type: String, required: true }, // 필수
-    group: { type: String, required: true }, // 필수
+    affiliation: { type: String, required: true }, // 필수
     giftAmount: { type: Number, default: null }, // 선택
     ticketCount: { type: Number, default: null }, // 선택
     note: { type: String, default: '' }, // 선택, 기본값은 빈 문자열
@@ -29,7 +33,8 @@ const guestSchema = new mongoose.Schema(
 );
 
 // Mongoose 모델 생성
-const Guest =
-  mongoose.models['Guest'] || mongoose.model<IGuest>('Guest', guestSchema);
+const BrideGuest =
+  mongoose.models['BrideGuest'] ||
+  mongoose.model<IBrideGuest>('BrideGuest', brideGuestSchema);
 
-export default Guest;
+export default BrideGuest;
