1365796728434eda6715193af33cb69826213be5
Choi Young Sun <sunn7931@naver.com>
2025-01-07 17:21:19 +0900

    feat: 하객 정보 post 백엔드 엔드포인트 추가

---
+++ src/pages/api/guests/bride/index.ts
@@ -0,0 +1,101 @@
+import BrideGuest, { IBrideGuest } from '@/db/models/brideGuest';
+import { NextApiResponse } from 'next';
+import { ExtendedNextApiRequest } from '@/api/authenticate';
+import authenticate from '@/api/authenticate';
+import OrderNumber from '@/db/models/orderNumber';
+
+type ResponseData = {
+  success: boolean;
+  message: string;
+  guest?: {
+    _id: string;
+    orderNumber?: number;
+    side: string;
+    guestName: string;
+    affiliation: string;
+    ticketCount: number;
+    note: string;
+  };
+  data?: IBrideGuest[];
+  createdAt?: string;
+  updatedAt?: string;
+};
+
+export default authenticate(async function handler(
+  req: ExtendedNextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  if (req.method === 'POST') {
+    const { side, guestName, affiliation, giftAmount, ticketCount, note } =
+      req.body;
+
+    console.log(req.user);
+    const userId = req.user.id; // 인증된 사용자 정보를 req.user에서 가져옴
+
+    try {
+      const existingGuest = await BrideGuest.findOne({
+        userId,
+        guestName,
+        affiliation,
+      });
+      if (existingGuest) {
+        return res.status(400).send({
+          success: false,
+          message: '이미 등록된 하객 정보입니다.',
+        });
+      }
+
+      // 새로운 하객 등록 로직
+
+      // 해당 userId와 side의 마지막 orderNumber 가져오기
+      let orderEntry = await OrderNumber.findOne({ userId, side });
+      // 초기 값 설정
+      if (!orderEntry) {
+        orderEntry = new OrderNumber({ userId, side, lastOrderNumber: 0 });
+      }
+
+      // 새로운 하객의 orderNumber 설정
+      const newOrderNumber = orderEntry.lastOrderNumber + 1;
+
+      const newGuest = await BrideGuest.create({
+        userId,
+        orderNumber: newOrderNumber,
+        side,
+        guestName,
+        affiliation,
+        giftAmount,
+        ticketCount,
+        note,
+      });
+
+      // 새로운 orderNumber를 OrderNumber 모델에 저장
+      orderEntry.lastOrderNumber = newOrderNumber;
+      await orderEntry.save();
+
+      res.status(200).json({
+        success: true,
+        message: '하객 등록 성공!',
+        guest: newGuest,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Server error' });
+    }
+  } else if (req.method === 'GET') {
+    const userId = req.user.id;
+
+    try {
+      const guests = await BrideGuest.find({ userId });
+      res.status(200).json({
+        success: true,
+        message: '조회 성공!',
+        data: guests,
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Server error' });
+    }
+  } else {
+    res.status(405).json({ success: false, message: 'Method Not Allowed' });
+  }
+});
