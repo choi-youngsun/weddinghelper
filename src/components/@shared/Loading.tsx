8ad862274482acb38cdb9b94703a033ea514aa7f
Choi Young Sun <sunn7931@naver.com>
2025-01-21 21:40:01 +0900

    design: 로딩 컴포넌트 수정

--- src/components/@shared/Loading.tsx
+++ src/components/@shared/Loading.tsx
@@ -2,13 +2,14 @@ import Image from 'next/image';
 
 export default function Loading() {
   return (
-    <div className="mx-auto my-auto animate-fadeInOut">
+    <div className="mx-auto my-auto animate-fadeInOut text-center text-text-gray">
       <Image
         src="/images/loading.png"
         alt="보석 아이콘"
         width={100}
         height={50}
       ></Image>
+      Loading...
     </div>
   );
 }
