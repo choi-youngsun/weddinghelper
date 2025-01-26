ec772b9902a3c0037d52c438a2a3d83b406668aa
Choi Young Sun <sunn7931@naver.com>
2025-01-27 00:50:25 +0900

    design: 설명 모달 디자인 개선 및 페이지 추가

--- src/components/@shared/HowToModal.tsx
+++ src/components/@shared/HowToModal.tsx
@@ -25,8 +25,12 @@ export default function HowToModal({ isOpen, onClose }: HowToModalProps) {
       text: '[관리자]에서 신랑, 신부측 하객을 각각 관리할 수 있습니다.',
     },
     {
+      img: '/images/excel.gif',
+      text: '하객 데이터를 엑셀 파일로 저장할 수도 있습니다.',
+    },
+    {
       img: '/images/numbering.gif',
-      text: '넘버링된 번호는 변하지 않습니다.\n축의금 봉투 등에 적어 정보 취합에 활용하세요!',
+      text: '넘버링된 번호는 축의금 봉투에 적어 정보 취합에 활용하세요!',
     },
   ];
 
@@ -38,43 +42,49 @@ export default function HowToModal({ isOpen, onClose }: HowToModalProps) {
     setCurrentPage((prev) => prev - 1);
   };
 
+  const handleClose = () => {
+    setCurrentPage(0);
+    onClose();
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <p className="my-[20px] text-center text-md-extraBold">
-        어떻게 사용하나요?
-      </p>
-      <Image
-        src={pages[currentPage].img}
-        width={700}
-        height={600}
-        alt="소속 설정 gif"
-        unoptimized
-      />
-      <div className="mt-[10px] flex items-center justify-between ">
-        <Button
-          onClick={handlePrevClick}
-          disabled={currentPage === 0}
-          buttonWidth="fitToChildren"
-          buttonHeight={40}
-          className="px-[10px]"
-        >
-          이전
-        </Button>
-        <p
-          className="text-center text-md-regular"
-          style={{ whiteSpace: 'pre-line' }}
-        >
-          {pages[currentPage].text}
+    <Modal isOpen={isOpen} onClose={handleClose}>
+      <div className="flex h-[530px] w-[760px] flex-col justify-between">
+        <p className="mt-[20px] text-center text-md-extraBold">
+          어떻게 사용하나요?
         </p>
-        <Button
-          onClick={handleNextClick}
-          disabled={currentPage === pages.length - 1}
-          buttonWidth="fitToChildren"
-          buttonHeight={40}
-          className="px-[10px]"
-        >
-          다음
-        </Button>
+        <Image
+          src={pages[currentPage].img}
+          width={750}
+          height={600}
+          alt="설명 gif"
+          unoptimized
+          className="mx-auto"
+          quality={100}
+        />
+        <div className="flex items-center justify-between ">
+          <Button
+            onClick={handlePrevClick}
+            disabled={currentPage === 0}
+            buttonWidth="fitToChildren"
+            buttonHeight={40}
+            className="px-[10px]"
+          >
+            이전
+          </Button>
+          <p className="text-center text-md-regular">
+            {pages[currentPage].text}
+          </p>
+          <Button
+            onClick={handleNextClick}
+            disabled={currentPage === pages.length - 1}
+            buttonWidth="fitToChildren"
+            buttonHeight={40}
+            className="px-[10px]"
+          >
+            다음
+          </Button>
+        </div>
       </div>
     </Modal>
   );
