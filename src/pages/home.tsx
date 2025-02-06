4c0aae11ddf42d95e815a7b7b4ea1a24156404a7
Choi Young Sun <sunn7931@naver.com>
2025-02-06 17:22:48 +0900

    design: 모바일 상단 여백 조정

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="mx-auto flex w-[200px] flex-col items-center justify-center gap-[20px]">
-      <Link href={'/admin'} className="mt-[300px] w-full ">
+      <Link href={'/admin'} className="mt-[260px] w-full md:mt-[300px] ">
         <Button buttonColor="white" borderColor="shadow">
           관리자
         </Button>
